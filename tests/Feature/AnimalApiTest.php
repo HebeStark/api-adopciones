868106aa91d81e351a59d85378794ce1b0960c8a
Hebe Stark <hebe_s@hotmail.com>
2026-01-28 18:06:06 +0100

    en test correccion de paginacion assertion en animal Api test

--- tests/Feature/AnimalApiTest.php
+++ tests/Feature/AnimalApiTest.php
@@ -3,18 +3,42 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Animal;
 
 class AnimalApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function it_can_list_animales()
+    {
+       Animal::factory()->count(3)->create();
+         $response = $this->getJson('/api/animales');
+         $response->assertStatus(200)
+        ->assertJsonCount(3, 'data');
+       
+    }
+      
+    /** @test */    
+    public function it_can_create_an_animal()
+    {
+      $data = [
+          'nombre' => 'Max',
+          'especie' => 'Perro',
+          'estado' => 'disponible',          
+      ];
+        $response = $this->postJson('/api/animales', $data);
+        $response->assertStatus(201)
+                 ->assertJsonFragment([
+                    'nombre' => 'Max',
+                    'especie' => 'Perro',
+                    'estado' => 'disponible', ]);
+
+        $this->assertDatabaseHas('animals', [
+            'nombre' => 'Max',
+            'especie' => 'Perro',
+            'estado' => 'disponible',
+        ]);
+
     }
 }
